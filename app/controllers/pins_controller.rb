b6e91f7a35ba4d98c0624aa1691480436a26a437
jivKrishna <krishnapandit1997@outlook.com>
2019-07-21 20:56:50 +0530

    2nd commit

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,46 @@
+class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @pin = Pin.new
+  end
+
+  def index
+    @pins = Pin.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: "Successfully created new Pin!"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Successfully updated the Pin!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @pin.destroy
+      redirect_to root_path, notice: "Successfully deleted one Pin!"
+    end
+  end
+
+  private
+  def pin_params
+    params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin 
+    @pin = Pin.find(params[:id])
+  end
+end
